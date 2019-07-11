772f4d3f9ac0da35fbd8d0817e5f3a060da39138
Andrei Barbu <andrei@0xab.com>
2019-07-11 17:58:32 -0400

    app two-stage authentication

--- app/App.js
+++ app/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, ActivityIndicator, StatusBar, AsyncStorage, Button, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createSwitchNavigator, createAppContainer, SafeAreaView } from "react-navigation";
+
 import Amplify from "aws-amplify";
+import { Auth } from "aws-amplify";
 import config from "./config.js";
+
+import Authentication from "./src/Authentication";
 
 Amplify.configure({
     Auth: {
@@ -10,6 +15,9 @@ Amplify.configure({
         userPoolId: config.cognito.USER_POOL_ID,
         identityPoolId: config.cognito.IDENTITY_POOL_ID,
         userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    Analytics: { 
+        disabled: true
     },
     API: {
         endpoints: [
@@ -28,19 +36,75 @@ Amplify.configure({
     }
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome to the app!',
+  };
+  render() {
+    return (
+      <SafeAreaView>
+        <Button title="Show me more of the app" onPress={this._showMoreApp} />
+        <Button title="Sign out" onPress={this._signOutAsync} />
+        <Button title="Sign out everywhere" onPress={this._signOutAsyncEverywhere} />
+      </SafeAreaView>
+    );
+  }
+  _showMoreApp = () => {
+    this.props.navigation.navigate('Other');
+  };
+  _signOutAsync = async () => {
+      Auth.signOut()
+          .then(data => this.props.navigation.navigate('Authentication'))
+          .catch(err => {
+              console.log(err)
+              this.props.navigation.navigate('Authentication')
+          }); // TODO What else can we do?
+  };
+  _signOutAsyncEverywhere = async () => {
+      Auth.signOut({global: true})
+          .then(data => this.props.navigation.navigate('Authentication'))
+          .catch(err => {
+              console.log(err)
+              this.props.navigation.navigate('Authentication')
+          }); // TODO What else can we do?
+  };
+}
+
+class OtherScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Lots of features here',
+  };
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Button title="I'm done, sign me out" onPress={this._signOutAsync} />
+        <StatusBar barStyle="default" />
+      </SafeAreaView>
+    );
+  }
+  _signOutAsync = async () => {
+      Auth.signOut()
+          .then(data => this.props.navigation.navigate('Authentication'))
+          .catch(err => this.props.navigation.navigate('Authentication')); // TODO What else can we do?
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+const AppStack = createStackNavigator({ Home: HomeScreen, Other: OtherScreen });
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    Authentication: Authentication,
+    App: AppStack
+  },
+  {
+    initialRouteName: 'Authentication'
+  }
+));
---
+++ app/src/Authentication.native.js
@@ -0,0 +1,153 @@
+import React from "react";
+import {
+  Platform,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  Modal,
+  Picker
+} from "react-native";
+import { Input, Button, ButtonGroup } from "react-native-elements";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+import { Auth } from "aws-amplify";
+var _ = require("lodash");
+var getYear = require("date-fns/get_year");
+import { isValid, parse } from "date-fns";
+import logo from "../assets/medicapt.png";
+
+import {
+  Authenticator,
+  SignIn,
+  SignUp,
+  ConfirmSignIn,
+  ConfirmSignUp,
+  Greetings,
+  VerifyContact,
+  ForgotPassword,
+  TOTPSetup,
+  Loading
+} from "aws-amplify-react-native";
+
+const AlwaysOn = props => {
+  return (
+    <View>
+      <Text>
+        I am always here to show current auth state: {props.authState}
+      </Text>
+      <Button onClick={() => props.onStateChange("signUp")}>
+        Show Sign Up
+      </Button>
+    </View>
+  );
+};
+
+export default class X extends React.Component {
+  handleAuthStateChange(state) {
+    if (state === "signedIn") {
+      /* Do something when the user has signed-in */
+    }
+  }
+  render() {
+    console.log("Running native version");
+    console.log(Platform.OS);
+    return (
+      <Authenticator
+        hideDefault={true}
+        onStateChange={this.handleAuthStateChange}
+      >
+        <SignIn />
+        <SignUp
+          signUpConfig={{
+            signUpFields: [
+              {
+                label: "Username",
+                key: "username",
+                required: true,
+                placeholder: "Username",
+                displayOrder: 1
+              },
+              {
+                label: "Email",
+                key: "email",
+                required: true,
+                placeholder: "Email",
+                type: "email",
+                displayOrder: 2
+              },
+              {
+                label: "Phone Number",
+                key: "phone_number",
+                placeholder: "Phone Number",
+                required: true,
+                displayOrder: 3
+              },
+              {
+                label: "Full name",
+                key: "name",
+                required: true,
+                placeholder: "Your full name",
+                displayOrder: 4
+              },
+              {
+                label: "What should we call you?",
+                key: "nickname",
+                required: true,
+                placeholder: "Your name",
+                displayOrder: 5
+              },
+              {
+                label: "Gender",
+                key: "gender",
+                required: true,
+                displayOrder: 6
+              },
+              {
+                label: "Birthdate",
+                key: "birthdate",
+                required: true,
+                type: "date",
+                displayOrder: 7
+              },
+              {
+                label: "Official ID Type",
+                key: "custom:official_id_type",
+                required: true,
+                displayOrder: 8
+              },
+              {
+                label: "Official ID Number",
+                key: "custom:official_id_number",
+                required: true,
+                displayOrder: 9
+              },
+              {
+                label: "Official ID Expiration",
+                key: "custom:official_id_expires",
+                required: true,
+                type: "date",
+                displayOrder: 10
+              },
+              {
+                label: "Password",
+                key: "password",
+                required: true,
+                placeholder: "Password",
+                type: "password",
+                displayOrder: 11
+              }
+            ]
+          }}
+        />
+        <ConfirmSignIn />
+        <ConfirmSignUp />
+        <Greetings />
+        <AlwaysOn />
+        <VerifyContact />
+        <ForgotPassword />
+        <Loading />
+      </Authenticator>
+    );
+  }
+}
