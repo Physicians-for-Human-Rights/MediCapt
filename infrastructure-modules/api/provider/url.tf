0ae357a66490a124bf874800a697310c28bafbb8
Andrei <andrei@0xab.com>
2022-01-27 03:13:46 -0500

    Clean up url files. Kept in history for reference

--- infrastructure-modules/api/provider/url.tf
+++ infrastructure-modules/api/provider/url.tf
@@ -1,31 +1,3 @@
-# resource "aws_acm_certificate" "cert" {
-#   domain_name       = aws_route53_zone.domain_zone.name
-#   validation_method = "DNS"
-
-#   subject_alternative_names = [
-#     "*.${aws_route53_zone.domain_zone.name}",
-#   ]
-
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-
-# # AWS needs to verify that we own the domain; to prove this we will create a
-# # DNS entry with a validation code
-# resource "aws_route53_record" "cert_validation_record" {
-#   name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
-#   type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
-#   records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
-#   zone_id = aws_route53_zone.domain_zone.zone_id
-#   ttl     = 60
-# }
-
-# resource "aws_acm_certificate_validation" "cert_validation" {
-#   certificate_arn         = aws_acm_certificate.cert.arn
-#   validation_record_fqdns = [aws_route53_record.cert_validation_record.fqdn]
-# }
-
 resource "aws_api_gateway_domain_name" "api_domain" {
   domain_name = "provider-api.${var.domain_name}"
   regional_certificate_arn = var.certificate_arn
@@ -54,6 +26,8 @@ resource "aws_route53_record" "api" {
     zone_id                = aws_api_gateway_domain_name.api_domain.regional_zone_id
   }
 
+  #NB: If we switch from a regional to a local endpoint we need to flip this too.
+  
   # alias {
   #   name                   = aws_api_gateway_domain_name.api_domain.cloudfront_domain_name
   #   zone_id                = aws_api_gateway_domain_name.api_domain.cloudfront_zone_id
