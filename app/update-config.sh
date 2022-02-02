100417690c8e31b93bea3d64b9e42a4e6701404f
Andrei <andrei@0xab.com>
2022-02-02 12:25:11 -0500

    Update the config.js file to use our custom URLs

--- app/update-config.sh
+++ app/update-config.sh
@@ -7,12 +7,12 @@ export P=../medicapt-infrastructure/$STAGE/$REGION
 export PROVIDER_USER_POOL_ID=$(terragrunt output              --terragrunt-working-dir $P/admin/users/provider cognito_user_pool_id)
 export PROVIDER_APP_CLIENT_ID=$(terragrunt output             --terragrunt-working-dir $P/admin/users/provider -json cognito_user_pool_client_web|jq '.id')
 export PROVIDER_IDENTITY_POOL_ID=$(terragrunt output          --terragrunt-working-dir $P/admin/users/provider -json cognito_identity_pool|jq '.id')
-export PROVIDER_URL=$(printf '"%s%s/"'  $(terragrunt output   --terragrunt-working-dir $P/api/provider -json aws_api_gateway_deployment_api|jq '.invoke_url'|cut -d '"' -f 2) $STAGE)
+export PROVIDER_URL=$(terragrunt output   --terragrunt-working-dir $P/api/provider -json aws_api_gateway_domain_name_id|cut -d '"' -f 2)
 
 export ASSOCIATE_USER_POOL_ID=$(terragrunt output             --terragrunt-working-dir $P/admin/users/associate cognito_user_pool_id)
 export ASSOCIATE_APP_CLIENT_ID=$(terragrunt output            --terragrunt-working-dir $P/admin/users/associate -json cognito_user_pool_client_web|jq '.id')
 export ASSOCIATE_IDENTITY_POOL_ID=$(terragrunt output         --terragrunt-working-dir $P/admin/users/associate -json cognito_identity_pool|jq '.id')
-export ASSOCIATE_URL=$(printf '"%s%s/"'  $(terragrunt output  --terragrunt-working-dir $P/api/associate -json aws_api_gateway_deployment_api|jq '.invoke_url'|cut -d '"' -f 2) $STAGE)
+export ASSOCIATE_URL=$(terragrunt output   --terragrunt-working-dir $P/api/associate -json aws_api_gateway_domain_name_id|cut -d '"' -f 2)
 
 
 envsubst < config.js.template > config.js
