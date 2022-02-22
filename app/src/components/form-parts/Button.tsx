6c13c59efa1a10438b1bd721f3f2a25c298ae87b
Andrei <andrei@0xab.com>
2022-02-22 14:36:41 -0500

    Add a button component for the renderer

---
+++ app/src/components/form-parts/Button.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Button as NButton } from 'native-base'
+import _ from 'lodash'
+
+export default function Button({
+  key,
+  text,
+  onPress,
+  colorScheme = 'blue',
+  maxW = '30%',
+  icon,
+}: {
+  key: string
+  text: string
+  onPress: () => any
+  colorScheme?: string
+  maxW?: string
+  icon?: JSX.Element
+}) {
+  return (
+    <NButton
+      key={key}
+      flex={1}
+      _text={{ bold: true }}
+      maxW={maxW}
+      onPress={onPress}
+      leftIcon={icon}
+    >
+      {text}
+    </NButton>
+  )
+}
