9108d91c5ebef57a69689456543df0707cd1dadd
Felipe Monsalve <felipemonsalve@dhcp-10-31-95-150.dyn.MIT.EDU>
2022-04-07 10:29:01 -0400

    added parts to RecordType

--- app/src/utils/types/record.tsx
+++ app/src/utils/types/record.tsx
@@ -6,27 +6,22 @@ export type RecordType = {
 }
 
 export type RecordSections = {
-  [path: string]: RecordSubparts
+  [sectionName: string]: RecordSection
 }
 
-export type RecordSubparts = {
+export type RecordSection = {
   parts: RecordParts
 }
 
 export type RecordParts = {
-  [path: string]: RecordDataContents
+  [partName: string]: RecordPart
 }
 
-export type RecordPath = (string | number)[]
+export type RecordPart = {
+  parts: RecordParts
+} & RecordDataContents
 
-// TODO Bring this back and test it
-// export type RecordData = RecordDataContents & {
-//   /**
-//    * @pattern ^[a-zA-Z_-]+$
-//    */
-//   path: string
-//   parts?: RecordDataByType['part'][]
-// }
+export type RecordPath = (string | number)[]
 
 export type RecordPhoto = {
   /**
@@ -39,109 +34,99 @@ export type RecordPhoto = {
   'date-taken': Date
 }
 
-export type RecordDataByType =
-  // TODO Bring this back and test it
-  // part: {
-  //   /**
-  //    * @pattern ^[a-zA-Z_-]+$
-  //    */
-  //   name: string
-  //   part: RecordData
-  // }
-  {
-    parts: RecordParts
-    bool: {
-      value: boolean
-    }
-    signature: {
-      signer?: string
-      /**
+export type RecordDataByType = {
+  bool: {
+    value: boolean
+  }
+  signature: {
+    signer?: string
+    /**
        @format uri
     */
-      uri: string
-      'date-signed': Date
-    }
-    date: {
-      birthdate?: boolean
-      value: Date
-    }
-    'date-time': {
-      /**
+    uri: string
+    'date-signed': Date
+  }
+  date: {
+    birthdate?: boolean
+    value: Date
+  }
+  'date-time': {
+    /**
      @format date-time
   */
-      value: Date
-    }
-    number: {
-      value: string
-    }
-    list: {
-      selection: string
-      other?: string
-    }
-    'list-with-labels': {
-      selection: string
-      other?: string
-    }
-    'list-multiple': {
-      value: (boolean | null)[]
-      other?: string
-    }
-    'list-with-labels-multiple': {
-      selections: string[]
-      other?: string
-    }
-    text: {
-      value: string
-    }
-    'long-text': {
-      value: string
-    }
-    email: {
-      /**
+    value: Date
+  }
+  number: {
+    value: string
+  }
+  list: {
+    selection: string
+    other?: string
+  }
+  'list-with-labels': {
+    selection: string
+    other?: string
+  }
+  'list-multiple': {
+    value: (boolean | null)[]
+    other?: string
+  }
+  'list-with-labels-multiple': {
+    selections: string[]
+    other?: string
+  }
+  text: {
+    value: string
+  }
+  'long-text': {
+    value: string
+  }
+  email: {
+    /**
            @faker internet.email
         */
-      value: string
-    }
-    address: {
-      value: string
-    }
-    'phone-number': {
-      value: string
-    }
-    gender: {
-      value: string
-    }
-    sex: {
-      value: string
-    }
-    photo: {
-      value: RecordPhoto[]
-    }
-    'body-image': {
-      /**
+    value: string
+  }
+  address: {
+    value: string
+  }
+  'phone-number': {
+    value: string
+  }
+  gender: {
+    value: string
+  }
+  sex: {
+    value: string
+  }
+  photo: {
+    value: RecordPhoto[]
+  }
+  'body-image': {
+    /**
        @format uri
     */
-      uri: string
-      annotations: {
-        location: {
-          /**
+    uri: string
+    annotations: {
+      location: {
+        /**
            @minimum 0
            @maximum 10000
            @type integer
         */
-          x: number
-          /**
+        x: number
+        /**
            @minimum 0
            @maximum 10000
            @type integer
         */
-          y: number
-        }
-        text: string
-        photos: RecordPhoto[]
-      }[]
-    }
+        y: number
+      }
+      text: string
+      photos: RecordPhoto[]
+    }[]
   }
+}
 
 // export type RecordDataMaybeRepeated<T extends keyof RecordDataByType> =
 export type RecordDataMaybeRepeated<T extends keyof RecordDataByType> =
@@ -153,7 +138,7 @@ export type RecordDataContents =
   | RecordDataMaybeRepeated<'bool'>
   | RecordDataMaybeRepeated<'signature'>
   | RecordDataMaybeRepeated<'date'>
-  | RecordDataMaybeRepeated<'date'>
+  | RecordDataMaybeRepeated<'date-time'>
   | RecordDataMaybeRepeated<'number'>
   | RecordDataMaybeRepeated<'list-multiple'>
   | RecordDataMaybeRepeated<'list-with-labels-multiple'>
@@ -166,6 +151,7 @@ export type RecordDataContents =
   | RecordDataMaybeRepeated<'sex'>
   | RecordDataMaybeRepeated<'photo'>
   | RecordDataMaybeRepeated<'body-image'>
+// | RecordDataMaybeRepeated<'list-with-parts'>
 
 // TODO Integrate this back in
 // export type RecordDataMap = {
