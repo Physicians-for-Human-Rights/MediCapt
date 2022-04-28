5d4cbbcd276b367e895631323e21c66cab773bf7
Felipe Monsalve <felipemonsalve@31-38-235.wireless.csail.mit.edu>
2022-04-28 17:10:51 -0400

    rewrote record type as zod schema

--- app/src/utils/types/record.tsx
+++ app/src/utils/types/record.tsx
@@ -1,5 +1,6 @@
 import { z } from 'zod'
 import { FormKVRawType, FormPart } from 'utils/types/form'
+import { dateSchema } from './common'
 
 export type RecordValuePath = (string | number)[]
 
@@ -8,6 +9,175 @@ export type RecordValue =
   | RecordValueByType['repeat-list']
   | RecordValueByType['list-with-parts']
 export type FlatRecord = Record<string, RecordValue>
+
+export const recordTypeSchema = z.lazy(() =>
+  z.object({
+    'storage-version': z.literal('1.0.0').default('1.0.0'),
+    sections: z.record(
+      z.object({
+        parts: z.record(skippableRecordPartSchema),
+      })
+    ),
+  })
+)
+
+export const skippableRecordPartSchema = z.lazy(() =>
+  z.intersection(
+    recordPartSchema,
+    z.object({
+      skipped: z.boolean().optional(),
+    })
+  )
+)
+
+export const recordPartSchema = z.lazy(() =>
+  z.union([
+    recordValueTypesSchema['repeat-list'],
+    recordValueTypesSchema['bool'],
+    recordValueTypesSchema['number'],
+    recordValueTypesSchema['text'],
+    recordValueTypesSchema['long-text'],
+    recordValueTypesSchema['email'],
+    recordValueTypesSchema['address'],
+    recordValueTypesSchema['phone-number'],
+    recordValueTypesSchema['gender'],
+    recordValueTypesSchema['sex'],
+    recordValueTypesSchema['date'],
+    recordValueTypesSchema['date-time'],
+    recordValueTypesSchema['singature'],
+    recordValueTypesSchema['photo'],
+    recordValueTypesSchema['body-image'],
+    recordValueTypesSchema['list'],
+    recordValueTypesSchema['list-with-labels'],
+    recordValueTypesSchema['list-multiple'],
+    recordValueTypesSchema['list-with-labels-multiple'],
+    recordValueTypesSchema['list-with-parts'],
+  ])
+)
+
+export const recordValueTypesSchema = {
+  'repeat-list': z.object({
+    type: z.literal('repeat-list'),
+    value: z.string().array(),
+  }),
+  bool: z.object({
+    type: z.literal('bool'),
+    value: z.boolean(),
+  }),
+  number: z.object({
+    type: z.literal('number'),
+    value: z.number(),
+  }),
+  text: z.object({
+    type: z.literal('text'),
+    value: z.string(),
+  }),
+  'long-text': z.object({
+    type: z.literal('long-text'),
+    value: z.string(),
+  }),
+  email: z.object({
+    type: z.literal('email'),
+    value: z.string().email().nonempty(),
+  }),
+  address: z.object({
+    type: z.literal('address'),
+    value: z.string().nonempty(),
+  }),
+  'phone-number': z.object({
+    type: z.literal('phone-number'),
+    value: z.string().nonempty(),
+  }),
+  gender: z.object({
+    type: z.literal('gender'),
+    value: z.string().nonempty(),
+  }),
+  sex: z.object({
+    type: z.literal('sex'),
+    value: z.enum(['male', 'female', 'intersex']),
+  }),
+  date: z.object({
+    type: z.literal('date'),
+    value: dateSchema,
+  }),
+  'date-time': z.object({
+    type: z.literal('date-time'),
+    value: dateSchema,
+  }),
+  singature: z.object({
+    type: z.literal('signature'),
+    value: z.string().nonempty(),
+    signer: z.string().optional(),
+    'date-signed': dateSchema.optional(),
+  }),
+  photo: z.object({
+    type: z.literal('photo'),
+    value: z.lazy(() => recordPhotoSchema.array()),
+  }),
+  'body-image': z.object({
+    type: z.literal('body-image'),
+    value: z.object({
+      uri: z.string(),
+      annotations: z.lazy(() => imageAnnotationSchema.array()),
+    }),
+  }),
+  list: z.object({
+    type: z.literal('list'),
+    value: z.object({
+      options: z
+        .union([z.string().array(), z.number().array(), z.boolean().array()])
+        .nullable(),
+      selection: z.string().nullable(),
+      otherValue: z.string().nullable(),
+    }),
+  }),
+  'list-with-labels': z.object({
+    type: z.literal('list-with-labels'),
+    value: z.object({
+      options: z.object({
+        key: z.string().nonempty(),
+        value: z.union([z.string(), z.number(), z.boolean()]).nullable(),
+      }),
+      selection: z.string().nullable(),
+      otherValue: z.string().nullable(),
+    }),
+  }),
+  'list-multiple': z.object({
+    type: z.literal('list-multiple'),
+    value: z.object({
+      options: z.union([
+        z.string().array(),
+        z.number().array(),
+        z.boolean().array(),
+      ]),
+      selections: z.boolean().array(),
+      otherChecked: z.boolean().optional(),
+      otherValue: z.string().optional(),
+    }),
+  }),
+  'list-with-labels-multiple': z.object({
+    type: z.literal('list-with-labels-multiple'),
+    value: z.string(),
+  }),
+  'list-with-parts': z.object({
+    type: z.literal('list-with-parts'),
+    value: z.string(),
+  }),
+}
+
+const recordPhotoSchema = z.object({
+  uri: z.string().nonempty(),
+  'date-taken': dateSchema.optional(),
+})
+
+const imageAnnotationSchema = z.object({
+  location: z.object({
+    x: z.number().int().min(0).max(10000),
+    y: z.number().int().min(0).max(10000),
+  }),
+  text: z.string(),
+  photos: recordPhotoSchema.array(),
+})
 
 export type RecordType = {
   'storage-version'?: '1.0.0'
@@ -233,12 +403,7 @@ export type ImageAnnotation = {
   photos: RecordPhoto[]
 }
 
-// TODO Integrate this back in
-// export type RecordDataMap = {
-//   [path: string]: RecordValue | RecordData
-// }
-
-export const recordMetadataSchema = z
+export const RecordMetadata = z
   .object({
     recordUUID: z.string().nonempty(),
     recordID: z.string().nonempty(),
@@ -263,13 +428,4 @@ export const recordMetadataSchema = z
   })
   .strict()
 
-export type RecordMetadata = z.infer<typeof recordMetadataSchema>
-
-export type RecordValueTypes =
-  | string
-  | number
-  | boolean
-  | string[]
-  | number[]
-  | boolean[]
-  | Date
+export type RecordMetadata = z.infer<typeof RecordMetadata>
