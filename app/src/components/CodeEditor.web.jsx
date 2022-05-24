226f7e7df857299620d7e0a9705ba562bdd45478
Andrei <andrei@0xab.com>
2022-05-24 14:00:24 -0400

    Downgrade codemirror, v5 works better with yaml

--- app/src/components/CodeEditor.web.jsx
+++ app/src/components/CodeEditor.web.jsx
@@ -1,24 +1,10 @@
 import CodeMirror from '@uiw/react-codemirror'
-import { StreamLanguage } from '@codemirror/stream-parser'
-import { yaml as yamlLang } from '@codemirror/legacy-modes/mode/yaml'
-import { defaultHighlightStyle } from '@codemirror/highlight'
-import { lineNumbers, highlightActiveLineGutter } from '@codemirror/gutter'
-import { searchKeymap, highlightSelectionMatches } from '@codemirror/search'
-import { autocompletion, completionKeymap } from '@codemirror/autocomplete'
-import { commentKeymap } from '@codemirror/comment'
-import {
-  keymap,
-  highlightSpecialChars,
-  drawSelection,
-  highlightActiveLine,
-  dropCursor,
-} from '@codemirror/view'
-import { closeBrackets, closeBracketsKeymap } from '@codemirror/closebrackets'
-import { defaultKeymap } from '@codemirror/commands'
-import { indentOnInput } from '@codemirror/language'
-import { history, historyKeymap } from '@codemirror/history'
-import { lintGutter, lintKeymap } from '@codemirror/lint'
-import { foldGutter, foldKeymap } from '@codemirror/fold'
+import 'codemirror/theme/idea.css'
+import 'codemirror/addon/search/search'
+import 'codemirror/addon/search/searchcursor'
+import 'codemirror/addon/search/jump-to-line'
+import 'codemirror/addon/dialog/dialog'
+import 'codemirror/addon/dialog/dialog.css'
 
 export default function CodeEditor({
   contents,
@@ -31,26 +17,14 @@ export default function CodeEditor({
       value={contents}
       height={Math.round(window.height * 0.85) + 'px'}
       width={Math.round(window.width * ratio) + 'px'}
-      extensions={[
-        StreamLanguage.define(yamlLang),
-        defaultHighlightStyle.fallback,
-        lineNumbers(),
-        highlightActiveLineGutter(),
-        indentOnInput(),
-        foldGutter(),
-        lintGutter(),
-        keymap.of([
-          ...closeBracketsKeymap,
-          ...defaultKeymap,
-          ...searchKeymap,
-          ...historyKeymap,
-          ...commentKeymap,
-          ...completionKeymap,
-          ...lintKeymap,
-          ...foldKeymap,
-        ]),
-      ]}
-      onChange={value => setRawContents(value)}
+      onChanges={editor => setRawContents(editor.getValue())}
+      options={{
+        theme: 'idea',
+        mode: 'yaml',
+        search: {
+          bottom: false,
+        },
+      }}
     />
   )
 }
