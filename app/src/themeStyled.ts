9757b265f9f83107660cf15b4b28fbbc21c3f7a6
SAKI HAYASHI <sh3467@nyu.edu>
2023-10-12 21:13:48 -0400

    updates

---
+++ app/src/themeStyled.ts
@@ -0,0 +1,113 @@
+import { StyleService } from '@ui-kitten/components'
+const themedStyles = StyleService.create({
+  py10: {
+    paddingTop: 40,
+    paddingBottom: 40,
+  },
+  pb2: {
+    paddingBottom: 8,
+  },
+  pt1: {
+    paddingTop: 4,
+  },
+  pt4: {
+    paddingTop: 16,
+  },
+  pl3: {
+    paddingLeft: 12,
+  },
+  ml1: {
+    marginLeft: 4,
+  },
+  ml2: {
+    marginLeft: 8,
+  },
+  mb3: {
+    marginBottom: 12,
+  },
+  mrMinus1: {
+    marginRight: -4,
+  },
+  fontBold: {
+    fontWeight: 'bold',
+  },
+  fontMedium: {
+    fontWeight: '500',
+  },
+  fontLight: {
+    fontWeight: '200',
+  },
+  fontSizeXs: {
+    fontSize: 12,
+  },
+  fontSizeSm: {
+    fontSize: 14,
+  },
+  fontSizeLg: {
+    fontSize: 18,
+  },
+  colorCoolGray900: {
+    color: '#111827',
+  },
+  colorCoolGray800: {
+    color: '#1f2937',
+  },
+  colorCoolGray700: {
+    color: '#374151',
+  },
+  colorCoolGray600: {
+    color: '#4b5563',
+  },
+  colorCoolGray500: {
+    color: '#6b7280',
+  },
+  colorCoolGray50: {
+    color: '#f9fafb',
+  },
+  textLeft: {
+    textAlign: 'left',
+  },
+  truncated: {
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+  },
+  width50: {
+    width: '50%',
+  },
+  width25: {
+    width: '25%',
+  },
+  width20: {
+    width: '20%',
+  },
+  width10: {
+    width: '10%',
+  },
+  width56x4: {
+    width: 56 * 4,
+  },
+  width64x4: {
+    width: 64 * 4,
+  },
+  width30Percent: {
+    width: '30%',
+  },
+  width25Percent: {
+    width: '25%',
+  },
+  width20Percent: {
+    width: '20%',
+  },
+  width10Percent: {
+    width: '10%',
+  },
+  maxWidth100Percent: {
+    maxWidth: '100%',
+  },
+  maxWidth60Percent: {
+    maxWidth: '60%',
+  },
+})
+
+export default themedStyles
